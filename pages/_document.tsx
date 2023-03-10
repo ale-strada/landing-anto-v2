8049845c80fbd2526e5dc2b71561d386cd054f41
ale-strada <79310707+ale-strada@users.noreply.github.com>
2023-03-10 13:03:56 -0300

    seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-        <Head/>
+        <Head>
+          <meta name="description" content="Antonella Digital Design"/>
+          <link rel="shortcut icon" href="/logo.png" />
+        </Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,6 @@ export default function Home() {
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
       <link rel="preconnect" href="https://fonts.gstatic.com"/>
       <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"/>
-      
-      <link rel="shortcut icon" href="/logo.png" />
       <title>Antonella Design</title>
     </Head>
     <div>
