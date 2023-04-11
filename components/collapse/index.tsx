e5f6ea59ab643d23e190da9b1ec2515e822f1b61
ale-strada <79310707+ale-strada@users.noreply.github.com>
2023-04-11 19:09:54 -0300

    buton to brand

--- components/collapse/index.tsx
+++ components/collapse/index.tsx
@@ -1,9 +1,10 @@
 import { ClosedArrow, OpenArrow } from '@/ui/img';
-import { CardDetailsText, TextRoboto700Negro } from '@/ui/text';
+import { CardDetailsText, TextInter600Lila, TextRoboto700Negro } from '@/ui/text';
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Collapse from 'react-bootstrap/Collapse';
 import { DetalleCardConteiner } from '../servicios/styled';
+import { ButtonPaquetes } from '@/ui/buttons/styled';
 
 export function CollapseCard(props:any) {
 const [open, setOpen] = useState(false);
@@ -24,10 +25,20 @@ return (
         
         </DetalleCardConteiner>
         <Collapse in={open}>
-        <div id="collapse-text">
-        <CardDetailsText>
-            {props.contenido}   
-        </CardDetailsText>
+        <div  id="collapse-text">
+            <div style={{display: "flex", flexDirection: "column"}}>
+                <CardDetailsText>
+                    {props.contenido}   
+                </CardDetailsText>
+                {props.paquete? <ButtonPaquetes onClick={props.onClick}>
+                    <TextInter600Lila style={{margin:"0"}}>Ver Paquetes</TextInter600Lila>
+                </ButtonPaquetes>
+                :
+                ""
+            }
+                
+            </div>
+        
         </div>
         </Collapse>
     </div>
