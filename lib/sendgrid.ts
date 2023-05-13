cf840d30b132e123996ca833c7967de517f13598
ale-strada <79310707+ale-strada@users.noreply.github.com>
2023-05-12 23:22:03 -0300

    test fix email

--- lib/sendgrid.ts
+++ lib/sendgrid.ts
@@ -1,14 +1,15 @@
 import sgMail from "@sendgrid/mail";
 sgMail.setApiKey(process.env.SENDGRID_API_KEY || "");
 
-export function sendEmail(mensaje: any) {
+export async function sendEmail(mensaje: any) {
   const msg = {
     to: "jaime.antonella29@gmail.com", // Change to your recipient
     from: "buscador.de.mascotas.app@gmail.com", // Change to your verified sender
     subject: "mensaje de la web",
     text: mensaje,
   };
-  sendEmailLib(msg);
+  //prueba
+  await sendEmailLib(msg);
 }
 
 async function sendEmailLib(mensaje: any) {
