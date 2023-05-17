fb4e9f1f18e2f33769d877a4c29717bf0e129bc3
ale-strada <79310707+ale-strada@users.noreply.github.com>
2023-05-17 17:01:32 -0300

    file upload limit size

--- components/file-input/index.tsx
+++ components/file-input/index.tsx
@@ -1,29 +1,47 @@
 import { fileBase64State, fileNameState } from '@/lib/hooks';
 import Form from 'react-bootstrap/Form';
 import { useRecoilState } from 'recoil';
+import Swal from 'sweetalert2';
 
 function FormFile() {
     const [fileName, setFileName] = useRecoilState(fileNameState)
     const [fileBase64, setFileBase64] = useRecoilState(fileBase64State)
     function handleSubmit(e:any){
         e.preventDefault()
-        setFileName(e.target.files[0].name)
+        if(e.target.files[0].size <= 2000000){
+
+            setFileName(e.target.files[0].name)
+            const file = e.target.files[0]; // tu objeto Fil
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            
+            /**
+             * Establece la cadena base64 del archivo en el estado después de leerla de un objeto de archivo.
+             *
+             * @param {Evento} event - El evento de entrada de archivo que contiene el objeto de archivo.
+             */
+            reader.onload = function () {
+                if (typeof reader.result === 'string') {
+                    const base64String = reader.result.replace('data:', '').replace(/^.+,/, '');
+                    setFileBase64(base64String);
+                }
+            };
+
+        }else{
+            Swal.fire({
+                title: "El archivo es demasiado grande. El tamaño debe ser menor a 2MB",
+                icon: "error",
+                confirmButtonText: "ok",
+            }).then(()=>{
+                setFileName("")
+                setFileBase64("")
+            })
+        }
         
-        const file = e.target.files[0]; // tu objeto Fil
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
         
-        /**
-         * Establece la cadena base64 del archivo en el estado después de leerla de un objeto de archivo.
-         *
-         * @param {Evento} event - El evento de entrada de archivo que contiene el objeto de archivo.
-         */
-        reader.onload = function () {
-            if (typeof reader.result === 'string') {
-                const base64String = reader.result.replace('data:', '').replace(/^.+,/, '');
-                setFileBase64(base64String);
-            }
-        };
+        
+        
+
         
     }
 
